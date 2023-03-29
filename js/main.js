2fbe621f7ac158ef9075bff56a84e701667f46ec
Slawone <for1s8@ya.ru>
2023-03-29 21:48:20 +0300

    add new branch

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const modalTitle = document.querySelector('.modal__title');
+const modalForm = document.querySelector('.modal');
+const checkboxItem = document.querySelector('.checkbox__item');
+const checkboxInput = document.querySelector('.checkbox__input');
+const modalPrice = document.querySelector('.modal__price');
+
+console.log(modalTitle);
+console.log(modalForm);
+console.log(checkboxItem);
+console.log(checkboxInput);
+console.log(modalPrice);
